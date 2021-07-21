45fbb900359da3e57c918d2934d48100aa8a06c3
JimmyShi22 <417711026@qq.com>
2021-07-21 12:13:42 +0800

    Add ci check
    (cherry picked from commit 413659f7510acff7a43d3f474ba3a5ade19a2de4)

--- .ci/ci_check.sh
+++ .ci/ci_check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -e
+PROXY_SRC_DIR=src/main/resources/chaincode/WeCrossProxy
+PROXY_DEST_DIR=src/test/resources/chains/fabric2/chaincode/WeCrossProxy/src/github.com/WeCrossProxy
 
 ./gradlew verifyGoogleJavaFormat
 
@@ -11,8 +13,12 @@ bash ../scripts/build_fabric_demo_chain.sh
 cp -r certs/*  ../src/test/resources/
 
 cd -
-
-cp -r src/main/resources/chaincode/WeCrossProxy src/test/resources/chains/fabric/WeCrossProxy
+mkdir -p ${PROXY_DEST_DIR}
+cp -r ${PROXY_SRC_DIR}/* ${PROXY_DEST_DIR}/
+cd ${PROXY_DEST_DIR}
+go get
+go mod vendor
+cd -
 
 ./gradlew build -x test
 ./gradlew test -i
